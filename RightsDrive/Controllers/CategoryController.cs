a6fa29b82560df70fe9d833a750ae8256a585c8d
Mark Traynor <markt@dropcap.com>
2020-03-13 12:41:04 -0500

    update

--- RightsDrive/Controllers/CategoryController.cs
+++ RightsDrive/Controllers/CategoryController.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
 
 namespace RightsDrive.Controllers
 {
     public class CategoryController : Controller
-    {
-        
+    {   
         public ActionResult Category(string category)
         {
-            ViewData["CatName"] = category;
+            string host = Request.Url.Host; //.Current.Request.Url.Host;
+            string url = "";
+            if (host.Contains("localhost"))
+                url = "https://localhost:44347/api/rh/category/namecheck?CategoryName=" + category;
+            else
+                url = "https://api.dropcap.com/api/rh/category/namecheck?CategoryName=" + category;
 
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "GET";
+            try
+            {
+                WebResponse response = request.GetResponse();
+            }
+            catch
+            {
+                //return HttpNotFound("File Not Found");
+                throw new HttpException(404, "File Not Found");
+            }
+
+            ViewData["CatName"] = category;
             return View();
         }
-
     }
 }
